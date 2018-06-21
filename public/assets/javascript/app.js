624a7dd36621e4341aa5f5c9f01911ed293d4b52
Timothy Dry <timothy.dry@verizon.net>
2018-06-21 17:54:24 -0400

    hunting down bug

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -30,8 +30,8 @@ $(document).ready(function () {
   });
 //-------- watch for clicks on Navigation items -----------------------------------------------------
   $(document).on('click', '.mynav', function () {
-    event.preventDefault();
-    var myX = false;
+    //event.preventDefault();
+    myX = false;
     $("#hamburger").html('<i class="fas fa-bars fa-sm"></i>');
     $('#navbarNavDropdown').removeClass('d-flex flex-row-reverse');
 //-------- collapse menu on moble devices after selection is made -----------------------------------
