53e948244fc21901e5d7eda98e5063a15a45052b
Timothy Dry <timothy.dry@verizon.net>
2018-07-31 17:18:09 -0400

    cleaned up meet page

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -168,11 +168,11 @@ var resultsJuly = ['','Loss','','Loss','Loss','','','']; //win or loss
 var scoreJuly = ['<br/>This meet is<br/>not scored','213.5 - 258.5','','240 - 277','219 - 280','','','<br/>This meet is<br/>not scored']; // meet score
 var linkJuly = ['https://drive.google.com/open?id=1ShYepR1wOOlTCtniZXCC-JjXrCHeyafg','https://drive.google.com/file/d/1cTL77wsGrmiGrF6WV7dCk_BLzB3aFqys/view?usp=sharing','','https://drive.google.com/open?id=1lAP0UM48V8EkLZpZWBiK-Zg57PAuXahv','https://drive.google.com/open?id=1w480zkzq6a3rTaXuta-lPFIlNWSGv54B','','https://d1nmxxg9d5tdo.cloudfront.net/3312/files/B_League_Championships_Results_2018.pdf?1532865893','https://swimtopia.s3.amazonaws.com/3312/files/conference_qual_times2.pdf?1521587981']; // link to external site for results
 var NresultsJune = ['RESULTS','RESULTS',''] // write RESULTS this turns on the results link on the page
-var NresultsJuly = ['RESULTS','RESULTS','','RESULTS','RESULTS','','','Qualifying<br />Times'];
+var NresultsJuly = ['RESULTS','RESULTS','','RESULTS','RESULTS','','',''];
 var NresultsAug = ['Qualifying<br />Times'];
 var linkAug = ['https://swimtopia.s3.amazonaws.com/3312/files/conference_qual_times2.pdf?1521587981']; // link to external site for results
 var CqualifyJuly = ['http://roycefield.org/sprint-results/','','','','','','https://d1nmxxg9d5tdo.cloudfront.net/3312/files/B_League_Championships_Results_2018.pdf?1532865893','https://drive.google.com/open?id=15OmiuHmgs4YHlU86pyMx15XYgmatnWxL']; //link to external site for qualifying times for conference meet
-var Nqualifying = ['ALL RESULTS','','','','','','All RESULTS','Tues. AM RESULTS'];
+var Nqualifying = ['ALL RESULTS','','','','','','All RESULTS','TUES. AM RESULTS'];
 var CqualifyAug = ['https://drive.google.com/file/d/1YtXYcRNBwL6Bl5CWPJbLM5uq7LcKhHLs/view?usp=sharing']; //link to external site for qualifying times for conference meet
 var NqualifyingAug = ['Qualifying<br />Swims'];
 //--------------------------------------------------------------------------------
